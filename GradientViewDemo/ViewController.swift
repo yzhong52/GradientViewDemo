9ec5d02177baadf975c0bdd4967699322d49107a
Yuchen Zhong <yuchen@hopper.com>
2018-02-24 11:23:13 -0500

    Initial Commit

---
+++ GradientViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GradientViewDemo
+//
+//  Created by Yuchen Zhong on 2018-02-24.
+//  Copyright © 2018 Yuchen Zhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
